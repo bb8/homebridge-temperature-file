bd936a747977a3d4d6837c193907f25246918683
Balázs Boros <balazs.boros@irithmics.com>
2025-03-28 14:10:27 +0000

    Reduce update frequency

--- index.js
+++ index.js
@@ -13,15 +13,22 @@ function TemperatureFileAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
   this.filePath = config["file_path"];
+  this.data = null;
 
   var service = new Service.TemperatureSensor(this.name);
 
   var changeAction = function(data) {
+    if (data == this.data || data === null)
+      return;
+  
+    log.info("Temperature changed: "+this.data+" -> "+data);
     service
       .getCharacteristic(Characteristic.CurrentTemperature)
       .setProps({unit: Characteristic.Units.CELSIUS, minValue: -100, maxValue: 100, minStep: 0.01})
       .setValue(data);
-  }
+
+    this.data = data;
+  }.bind(this);
 
   var changeHandler = function(path, stats) {
     fs.readFile(this.filePath, 'utf8', function(err, data) {
